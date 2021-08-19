d45514349e1ce04214d4d4eb0d662059fb3aa623
SOCINFORM\Anton.Klimantov <anton.klimantov@onlineinterviewer.ru>
2021-08-19 11:02:51 +0300

    check env

--- src/hooks/useSuperagent.ts
+++ src/hooks/useSuperagent.ts
@@ -7,6 +7,8 @@ type MethodType = 'GET' | 'POST'
 const baseUrl =
   process.env.NODE_ENV === 'development' ? 'http://10.10.4.72:30101/v1/admin/' : '/api/v1/admin/'
 
+console.log(process.env.NODE_ENV)
+
 export const useSuperagent = (url = '', method?: MethodType) => {
   const { replace } = useHistory()
 

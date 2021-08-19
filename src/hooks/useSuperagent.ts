82172efaec7a7a83463ca5e85342355e169324e0
SOCINFORM\Anton.Klimantov <anton.klimantov@onlineinterviewer.ru>
2021-08-19 10:51:25 +0300

    Поменять baseUrl

--- src/hooks/useSuperagent.ts
+++ src/hooks/useSuperagent.ts
@@ -4,7 +4,10 @@ import React from 'react'
 
 type MethodType = 'GET' | 'POST'
 
-const baseUrl = 'http://10.10.4.72:30101/v1/admin/'
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://10.10.4.72:30101/v1/admin/'
+    : '/'
 
 export const useSuperagent = (url = '', method?: MethodType) => {
   const { replace } = useHistory()

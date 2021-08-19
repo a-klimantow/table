e6ca0c589a0df82e664aca1cbc9e97fc0d075e84
SOCINFORM\Anton.Klimantov <anton.klimantov@onlineinterviewer.ru>
2021-08-19 11:05:59 +0300

    change baseUrl

--- src/hooks/useSuperagent.ts
+++ src/hooks/useSuperagent.ts
@@ -5,9 +5,9 @@ import React from 'react'
 type MethodType = 'GET' | 'POST'
 
 const baseUrl =
-  process.env.NODE_ENV === 'development' ? 'http://10.10.4.72:30101/v1/admin/' : '/api/v1/admin/'
+  process.env.NODE_ENV === 'development' ? 'http://10.10.4.72:30101/v1/admin/' : ''
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV, baseUrl)
 
 export const useSuperagent = (url = '', method?: MethodType) => {
   const { replace } = useHistory()

1c79a1c2931b1e54cca81a779370fb74bd25ba33
Антон Климантов <klimantow@yandex.ru>
2021-10-16 01:52:01 +0300

    Скрыть меню выплат

--- src/components/app_menu/index.tsx
+++ src/components/app_menu/index.tsx
@@ -1,3 +1,5 @@
+import { Route } from 'react-router-dom'
+
 import { rewards } from './menus'
 import { Provider, Menu, MenuItem } from './atoms'
 import { useHiddenMenu } from './hooks'
@@ -5,10 +7,12 @@ import { useHiddenMenu } from './hooks'
 export const AppMenu = () =>
   useHiddenMenu() ? null : (
     <Provider>
-      <Menu>
-        {rewards.map((item) => (
-          <MenuItem key={item.type + item.name} item={item} />
-        ))}
-      </Menu>
+      <Route path="/rewards/">
+        <Menu>
+          {rewards.map((item) => (
+            <MenuItem key={item.type + item.name} item={item} />
+          ))}
+        </Menu>
+      </Route>
     </Provider>
   )

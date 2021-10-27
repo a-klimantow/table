916af250623ff9e8e4b65f4874ce231174a1445b
Вячеслав Петров <Vyacheslav.Petrov@socinform.lan>
2021-10-27 16:16:22 +0300

    Пускать пользователей в админку у которых есть доступ и имеется написанный функционал PR-4195

--- src/hooks/fetch/useFetchLogin.ts
+++ src/hooks/fetch/useFetchLogin.ts
@@ -2,25 +2,51 @@ import * as React from 'react'
 import { ResponseError } from 'superagent'
 
 import { IErrors } from 'types'
-import { useToken, useUser, useFetch } from 'hooks'
+import { useToken, useUser, useFetch, useNotifications } from 'hooks'
 import { useLoginForm } from 'pages/login/hooks'
-
 type F = ReturnType<typeof useLoginForm>
 
 export function useFetchLogin(form: F) {
   const token = useToken()
   const user = useUser()
   const fetch = useFetch('login', 'post')
+  const ntf = useNotifications()
 
   fetch.send(form.data)
+
+  const RoleHaveAccess = [
+    'AccrualsManager',
+    'PaymentsManager'
+  ]
+
+  const intersect = (arr1: string[], arr2: string[]) => {
+    let result = false
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j< arr2.length; j++) {
+        if (arr1[i] === arr2[j]) {
+          result = true
+        }
+      }
+    }
+    return result
+  }
+
+  const checkUserRole = (body: any) => {
+    if (intersect(RoleHaveAccess, body.roles)) {
+      token.update(body)
+      user.update(body)
+    } else {
+      form.loading = false
+      return ntf.warning('Контент не доступен')
+    }
+  }
 
   React.useEffect(() => {
     form.loading &&
       (async () => {
         try {
           const { body } = await fetch
-          token.update(body)
-          user.update(body)
+          checkUserRole(body)
         } catch (error) {
           const { response } = error as ResponseError
           if (response?.body) {

5c43e2ee63b03a2c032f33b8fafac032f439bae0
Антон Климантов <klimantow@yandex.ru>
2021-11-01 15:24:52 +0300

    Изменить хук

--- src/components/table/table/hooks.ts
+++ src/components/table/table/hooks.ts
@@ -1,26 +1,32 @@
 import * as React from 'react'
 
+import { ICol as C } from '../types'
 import { TableState } from './state'
-import { columns } from '../data'
+
 type S = 'local' | 'session'
 type D = string | number | object
 
-export const useStorage = (key: string, st: S) => {
+export const useStorage = (st: S, key?: string) => {
   const storage = st === 'local' ? localStorage : sessionStorage
 
   return React.useMemo(
     () => ({
-      get<T extends D>(d?: T): T {
+      get<T extends D>(d: T): T {
+        if (typeof key === 'undefined') return d
+
         const res = storage.getItem(key)
-        return res ? JSON.parse(res) : d ?? null
+        return res ? JSON.parse(res) : d
       },
       set<T extends D>(d: T) {
+        if (typeof key === 'undefined') return
         storage.setItem(key, JSON.stringify(d))
       },
       remove() {
+        if (typeof key === 'undefined') return
         storage.removeItem(key)
       },
       clear() {
+        if (typeof key === 'undefined') return
         storage.clear()
       },
     }),
@@ -28,12 +34,12 @@ export const useStorage = (key: string, st: S) => {
   )
 }
 
-export function useTable() {
+export function useTable(columns: C[], key?: string) {
   return React.useRef(
     new TableState(
       columns,
-      useStorage('key', 'local'),
-      useStorage('key', 'session')
+      useStorage('local', key),
+      useStorage('session', key)
     )
   ).current
 }
